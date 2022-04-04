5f8f274c7ca8292232c3514d510be4e88baff313
Josef Atoui <josef.atoui@live.se>
2022-04-04 15:41:20 +0200

    Add data tables to match

--- lib/models/match.dart
+++ lib/models/match.dart
@@ -1,44 +1,79 @@
 import 'package:flutter/cupertino.dart';
 import 'package:yatzy/models/player.dart';
+import 'package:yatzy/models/yatzy_board.dart';
 
 class Match {
   UniqueKey? id;
   List<Player> players = [];
   Duration elpasedTime = Duration.zero;
-  int turn = 0;
+  int turn = 0, maxTurns = scoreLabel.values.length;
+  int? _currentPlayerIndex;
   Player? _currentPlayer;
-  // TODO: Add data tables
+  Player? winner;
 
   Match() {
     id = UniqueKey();
   }
 
-  void addPlayer(Player player) {
-    players.add(player);
+  @override
+  String toString() {
+    String t = "";
+    t += "* players: $players";
+    t += "\n* Current round: $turn";
+    t += "\n* Current Players turn: $_currentPlayer";
+
+    return t;
   }
 
+  void addPlayer(Player player) => players.add(player);
+
+  void addPlayers(List<Player> players) => this.players.addAll(players);
+
   Player? get currentPlayer => _currentPlayer;
+
+  void setCurrentPlayer(int index) {
+    assert(index <= players.length - 1);
+    _currentPlayer = players.elementAt(index);
+  }
 
   void removePlayer(UniqueKey id) {
     players.removeWhere((player) => player.id == id);
   }
 
   void startMatch() {
-    turn = 0;
+    turn = 1;
     _currentPlayer = players.first;
+    _currentPlayerIndex = 0;
   }
 
-  void finishMatch() {
+  void _finishMatch() {
+    Player? winner = players.fold<Player>(
+      players.first,
+      (previousPlayer, player) =>
+          player.board.score > previousPlayer.board.score
+              ? player
+              : previousPlayer,
+    );
     // TODO: Pick Winner
     // TODO: Store statistics
   }
 
-  void nextPlayer(int index) {
-    _currentPlayer = players.elementAt(index);
+  bool nextPlayer() {
+    if (currentPlayer == players.last) {
+      return false;
+    }
+    _currentPlayerIndex = _currentPlayerIndex! + 1;
+    _currentPlayer = players.elementAt(_currentPlayerIndex!);
+    return true;
   }
 
   void nextTurn() {
+    if (turn == maxTurns) {
+      _finishMatch();
+      return;
+    }
     turn++;
     _currentPlayer = players.first;
+    _currentPlayerIndex = 0;
   }
 }
